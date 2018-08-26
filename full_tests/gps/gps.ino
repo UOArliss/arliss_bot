c92e5b2c5591b0ee66c8f9de2ba5373045dc26c6
pork3 <zbower1@gmail.com>
2018-08-25 23:17:08 -0700

    added basic test for logging to sdcard

--- full_tests/gps/gps.ino
+++ full_tests/gps/gps.ino
@@ -1,91 +1,180 @@
+    /******************************************************************************
+  CSV_Logger_TinyGPSPlus.ino
+  Log GPS data to a CSV file on a uSD card
+  By Jim Lindblom @ SparkFun Electronics
+  February 9, 2016
+  https://github.com/sparkfun/GPS_Shield
+
+  This example uses SoftwareSerial to communicate with the GPS module on
+  pins 8 and 9, then communicates over SPI to log that data to a uSD card.
+
+  It uses the TinyGPS++ library to parse the NMEA strings sent by the GPS module,
+  and prints interesting GPS information - comma separated - to a newly created
+  file on the SD card.
+
+  Resources:
+  TinyGPS++ Library  - https://github.com/mikalhart/TinyGPSPlus/releases
+  SD Library (Built-in)
+  SoftwareSerial Library (Built-in)
+
+  Development/hardware environment specifics:
+  Arduino IDE 1.6.7
+  GPS Logger Shield v2.0 - Make sure the UART switch is set to SW-UART
+  Arduino Uno, RedBoard, Pro, etc.
+******************************************************************************/
+
+#include <SPI.h>
+#include <SD.h>
 #include "TinyGPS++.h"
-/* 
-   This sample sketch should be the first you try out when you are testing a TinyGPS++
-   (TinyGPSPlus) installation.  In normal use, you feed TinyGPS++ objects characters from
-   a serial NMEA GPS device, but this example uses static strings for simplicity.
-*/
 
-// A sample NMEA stream.
-const char *gpsStream =
-  "$GPRMC,045103.000,A,3014.1984,N,09749.2872,W,0.67,161.46,030913,,,A*7C\r\n"
-  "$GPGGA,045104.000,3014.1985,N,09749.2873,W,1,09,1.2,211.6,M,-22.5,M,,0000*62\r\n"
-  "$GPRMC,045200.000,A,3014.3820,N,09748.9514,W,36.88,65.02,030913,,,A*77\r\n"
-  "$GPGGA,045201.000,3014.3864,N,09748.9411,W,1,10,1.2,200.8,M,-22.5,M,,0000*6C\r\n"
-  "$GPRMC,045251.000,A,3014.4275,N,09749.0626,W,0.51,217.94,030913,,,A*7D\r\n"
-  "$GPGGA,045252.000,3014.4273,N,09749.0628,W,1,09,1.3,206.9,M,-22.5,M,,0000*6F\r\n";
+#define ARDUINO_USD_CS 10 
 
-// The TinyGPS++ object
-TinyGPSPlus gps;
+#define LOG_FILE_PREFIX "gpslog" 
+#define MAX_LOG_FILES 100 
+#define LOG_FILE_SUFFIX "csv" 
+
+char logFileName[13]; // Char string to store the log file name
+// Data to be logged:
+#define LOG_COLUMN_COUNT 8
+char * log_col_names[LOG_COLUMN_COUNT] = {
+  "longitude", "latitude", "altitude", "speed", "course", "date", "time", "satellites"
+};
+
+#define LOG_RATE 5000
+unsigned long lastLog = 0; 
+
+TinyGPSPlus tinyGPS; // tinyGPSPlus object to be used throughout
+#define GPS_BAUD 9600 // GPS module's default baud rate
+
+
+#include <SoftwareSerial.h>
+#define ARDUINO_GPS_RX 9 // GPS TX, Arduino RX pin
+#define ARDUINO_GPS_TX 8 // GPS RX, Arduino TX pin
+SoftwareSerial ssGPS(ARDUINO_GPS_TX, ARDUINO_GPS_RX); // Create a SoftwareSerial
+
+// Set gpsPort to either ssGPS if using SoftwareSerial or Serial1 if using an
+// Arduino with a dedicated hardware serial port
+#define gpsPort ssGPS  // Alternatively, use Serial1 on the Leonardo
+
+// Define the serial monitor port. On the Uno, and Leonardo this is 'Serial'
+//  on other boards this may be 'SerialUSB'
+#define SerialMonitor Serial
 
 void setup()
 {
-  Serial.begin(115200);
+  SerialMonitor.begin(9600);
+  gpsPort.begin(GPS_BAUD);
 
-  Serial.println(F("BasicExample.ino"));
-  Serial.println(F("Basic demonstration of TinyGPS++ (no device needed)"));
-  Serial.print(F("Testing TinyGPS++ library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
-  Serial.println(F("by Mikal Hart"));
-  Serial.println();
-
-  while (*gpsStream)
-    if (gps.encode(*gpsStream++))
-      displayInfo();
-
-  Serial.println();
-  Serial.println(F("Done."));
+  SerialMonitor.println("Setting up SD card.");
+  // see if the card is present and can be initialized:
+  if (!SD.begin(ARDUINO_USD_CS))
+  {
+    SerialMonitor.println("Error initializing SD card.");
+  }
+  updateFileName(); // Each time we start, create a new file, increment the number
+  printHeader(); // Print a header at the top of the new file
 }
 
 void loop()
 {
+  if ((lastLog + LOG_RATE) <= millis())
+  { // If it's been LOG_RATE milliseconds since the last log:
+    if (tinyGPS.location.isUpdated()) // If the GPS data is vaild
+    {
+      if (logGPSData()) // Log the GPS data
+      {
+        SerialMonitor.println("GPS logged."); // Print a debug message
+        lastLog = millis(); // Update the lastLog variable
+      }
+      else // If we failed to log GPS
+      { // Print an error, don't update lastLog
+        SerialMonitor.println("Failed to log new GPS data.");
+      }
+    }
+    else // If GPS data isn't valid
+    {
+      // Print a debug message. Maybe we don't have enough satellites yet.
+      SerialMonitor.print("No GPS data. Sats: ");
+      SerialMonitor.println(tinyGPS.satellites.value());
+    }
+  }
+
+  // If we're not logging, continue to "feed" the tinyGPS object:
+  while (gpsPort.available())
+    tinyGPS.encode(gpsPort.read());
 }
 
-void displayInfo()
+byte logGPSData()
 {
-  Serial.print(F("Location: ")); 
-  if (gps.location.isValid())
-  {
-    Serial.print(gps.location.lat(), 6);
-    Serial.print(F(","));
-    Serial.print(gps.location.lng(), 6);
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
+  File logFile = SD.open(logFileName, FILE_WRITE); // Open the log file
+
+  if (logFile)
+  { // Print longitude, latitude, altitude (in feet), speed (in mph), course
+    // in (degrees), date, time, and number of satellites.
+    logFile.print(tinyGPS.location.lng(), 6);
+    logFile.print(',');
+    logFile.print(tinyGPS.location.lat(), 6);
+    logFile.print(',');
+    logFile.print(tinyGPS.altitude.feet(), 1);
+    logFile.print(',');
+    logFile.print(tinyGPS.speed.mph(), 1);
+    logFile.print(',');
+    logFile.print(tinyGPS.course.deg(), 1);
+    logFile.print(',');
+    logFile.print(tinyGPS.date.value());
+    logFile.print(',');
+    logFile.print(tinyGPS.time.value());
+    logFile.print(',');
+    logFile.print(tinyGPS.satellites.value());
+    logFile.println();
+    logFile.close();
+
+    return 1; // Return success
   }
 
-  Serial.print(F("  Date/Time: "));
-  if (gps.date.isValid())
-  {
-    Serial.print(gps.date.month());
-    Serial.print(F("/"));
-    Serial.print(gps.date.day());
-    Serial.print(F("/"));
-    Serial.print(gps.date.year());
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
+  return 0; // If we failed to open the file, return fail
+}
 
-  Serial.print(F(" "));
-  if (gps.time.isValid())
-  {
-    if (gps.time.hour() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.hour());
-    Serial.print(F(":"));
-    if (gps.time.minute() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.minute());
-    Serial.print(F(":"));
-    if (gps.time.second() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.second());
-    Serial.print(F("."));
-    if (gps.time.centisecond() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.centisecond());
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
+// printHeader() - prints our eight column names to the top of our log file
+void printHeader()
+{
+  File logFile = SD.open(logFileName, FILE_WRITE); // Open the log file
 
-  Serial.println();
+  if (logFile) // If the log file opened, print our column names to the file
+  {
+    int i = 0;
+    for (; i < LOG_COLUMN_COUNT; i++)
+    {
+      logFile.print(log_col_names[i]);
+      if (i < LOG_COLUMN_COUNT - 1) // If it's anything but the last column
+        logFile.print(','); // print a comma
+      else // If it's the last column
+        logFile.println(); // print a new line
+    }
+    logFile.close(); // close the file
+  }
+}
+
+// updateFileName() - Looks through the log files already present on a card,
+// and creates a new file with an incremented file index.
+void updateFileName()
+{
+  int i = 0;
+  for (; i < MAX_LOG_FILES; i++)
+  {
+    memset(logFileName, 0, strlen(logFileName)); // Clear logFileName string
+    // Set logFileName to "gpslogXX.csv":
+    sprintf(logFileName, "%s%d.%s", LOG_FILE_PREFIX, i, LOG_FILE_SUFFIX);
+    if (!SD.exists(logFileName)) // If a file doesn't exist
+    {
+      break; // Break out of this loop. We found our index
+    }
+    else // Otherwise:
+    {
+      SerialMonitor.print(logFileName);
+      SerialMonitor.println(" exists"); // Print a debug statement
+    }
+  }
+  SerialMonitor.print("File name: ");
+  SerialMonitor.println(logFileName); // Debug print the file name
 }
