b36f119ce142446c1822fd71205b5be67aa999f2
mhernan <39255541+margielamadman@users.noreply.github.com>
2018-08-27 19:55:36 -0700

    Create skeleton for final software

---
+++ mainprocess.ino
@@ -0,0 +1,27 @@
+/********** Libraries **********/
+/********** Globals **********/
+/********** Setup **********/
+void setup() {
+	//Create all objects and variables needed
+		//Motors
+		//GPS
+		//Accelerometer
+		//Compass
+	//Initialize variables
+}
+
+/********** Loop **********/
+void loop() {
+	//Start standby mode
+		//Wait for launch trigger
+	//Launch triggered
+		//Have I landed?
+	//Landing confirmed
+		//Burn parachute
+		//Deploy Tail
+		//Roll forward (couple feet?)
+	//Get GPS signal
+		//Get compass heading
+		//Navigate towards waypoint
+	//Avoid obstacles
+}
