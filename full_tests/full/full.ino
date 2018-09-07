d84122584eb7f752e22899a5c88f2b8eac0444d4
Chase Craig <mylifeis2cool.cmc@gmail.com>
2018-09-07 14:14:12 -0700

    Added Full Folder
    Added folder for final code to be used when running the arduino.

---
+++ full_tests/full/full.ino
@@ -0,0 +1,19 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+#include "compass.h"
+LSM303 compass;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Wire.begin();
+  compass.init();
+  compass.enableDefault();
+}
+float t = 0;
+void loop() {
+  // put your main code here, to run repeatedly:
+  compass.read();
+  t = compass.heading();//Compass in degrees.
+  
+}
